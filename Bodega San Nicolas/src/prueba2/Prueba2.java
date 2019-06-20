106b9ba189cf4864ef10306f6315adb936a40920
Alacalufe1997 <diegolopra@gmail.com>
2019-06-20 11:23:03 -0400

    Update Prueba2.java
    cambios

--- Bodega San Nicolas/src/prueba2/Prueba2.java
+++ Bodega San Nicolas/src/prueba2/Prueba2.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package prueba2;
 
-/**
- *
- * @author RLCR
- */
+
 public class Prueba2 {
-    
+  
+    public static void main(String[] args) {
+        
+        System.out.println("Hola");
+        System.out.println("Mundo");
+        System.out.println("Cruel");
+    }
+  
 }
