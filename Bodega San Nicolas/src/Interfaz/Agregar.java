b99adf78031d6706b8ad5386c59be485ad64d0de
6runy <brunydemian10@gmail.com>
2019-06-24 10:43:04 -0400

    Correccion de licencia de todas las pestañas

--- Bodega San Nicolas/src/Interfaz/Agregar.java
+++ Bodega San Nicolas/src/Interfaz/Agregar.java
@@ -1,4 +1,4 @@
-//Copyright 2019 Nicolas Lobos Carcamo,Diego Parada
+//Copyright 2019 Nicolas Lobos Carcamo,Diego Parada Sepulveda,Bruny Vidal Lazzaro
 //
 //Licensed under the Apache License, Version 2.0 (the "License");
 //you may not use this file except in compliance with the License.
